64dea512197a447f4b25704bf277013312481467
Samah Shahla <samahch6@gmail.com>
2022-09-10 23:26:08 +0300

    adding async function to get the data of a api url

--- myscript.js
+++ myscript.js
@@ -2,6 +2,16 @@ const dogResult = document.getElementById('dog-result');
 const enteredName = document.getElementById('name');
 const submit_button = document.getElementById('submit-btn');
 
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+}
+
 window.addEventListener = ('load', getDogResult())
 
 function getDogResult() {
@@ -9,23 +19,26 @@ function getDogResult() {
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(res => res.json())
         .then(data => {
+            console.log(data.status);
             if (data.status == "success") {
-                dogResult.innerHTML = `<img src="${data.message}"/>`
+                dogResult.innerHTML = `<img src="${data.message}"/>`;
             }
             else {
-                alert("error, can't find image")
-            }
-        })
+                alert("error, can't find image");
+            };
+        });
 };
 
 submit_button.onclick =
     function getInputValue() {
-        console.log("button clicked")
-        var input_text = document.querySelector('#iname')
-        console.log(input_text.value)
+        var input_text = document.querySelector('#iname');
+        console.log(input_text.value);
+    };
 
+    const api_url = 
+      "https://api.agify.io/?name=input_text.value";
+
+     getapi(api_url);
 
     
-}
-
 
