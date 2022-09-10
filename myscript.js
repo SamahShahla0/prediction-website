cae7dbd6cdbcc6ac5deba8c830f1d939ffa6c31f
Samah Shahla <samahch6@gmail.com>
2022-09-10 19:40:00 +0300

    adding evet listener for button submit

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
 const dogResult = document.getElementById('dog-result')
 const enteredName = document.getElementById('name')
-//const submit_button = documnet.getElementById('submit-btn')
-window.addEventListener = ('load', getDogResult())
+const submit_button = document.getElementById('submit-btn')
 
+window.addEventListener = ('load', getDogResult())
 
 function getDogResult() {
     console.log('page is fully loaded');
@@ -12,3 +12,16 @@ function getDogResult() {
             dogResult.innerHTML = `<img src="${data.message}"/>`
         })
 };
+
+submit_button.addEventListener('click',getAllData())
+
+function getAllData(){
+    function getGender() {
+        console.log('page is fully loaded');
+        fetch('https://api.genderize.io/?name=rio')
+            .then(res => res.json())
+            .then(data => {
+                dogResult.innerHTML = `<img src="${data.message}"/>`
+            })
+    };
+}
