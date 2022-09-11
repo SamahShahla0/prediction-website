8b681f7a9238bb0b189106cec88f3d61d645a9d2
Samah Shahla <samahch6@gmail.com>
2022-09-11 07:25:28 +0300

    adding flag to view results once

--- myscript.js
+++ myscript.js
@@ -28,56 +28,63 @@ function getDogResult() {
         });
 };
 
+var predicted = 0;
 submit_button.onclick =
     function getResults() {
-        var results="";
-        const output_results = document.createElement("p")
+        
 
-        var input_text = document.querySelector('#iname');
-        new_name = input_text.value;
+        if (predicted == 0 ){
+            predicted = 1;
+            var results="";
+            const output_results = document.createElement("p")
 
-        const api_url1 = "https://api.agify.io/?name="+new_name;
-        const age = getapi(api_url1);
-        const getAge= () => {
-            age.then((a) => {
-                results = "\nPredicted age is: " +a.age + "\n";
-                var y = document.createTextNode(results);
-                output_results.appendChild(y);
-               
-            });
-        };
+            var input_text = document.querySelector('#iname');
+            new_name = input_text.value;
 
-        getAge();
-
-
-        const api_url2 = "https://api.genderize.io/?name="+new_name;
-        const gender = getapi(api_url2);
-        const getGender= () => {
-            gender.then((a) => {
-                results =  "\nPredicted gender is: " +a.gender + " with probability: " + a.probability;
-                var x = document.createTextNode(results);
-                output_results.appendChild(x);
+            const api_url1 = "https://api.agify.io/?name="+new_name;
+            const age = getapi(api_url1);
+            const getAge= () => {
+                age.then((a) => {
+                    results = "\nPredicted age is: " +a.age + "\n";
+                    var y = document.createTextNode(results);
+                    output_results.appendChild(y);
                 
-            });
-        };
+                });
+            };
 
-        getGender();
+            getAge();
 
 
-        const api_url3 = "https://api.nationalize.io/?name="+new_name;
-        const nationality = getapi(api_url3);
-        const getNationality= () => {
-            nationality.then((a) => {
-                results =   "\nPredicted nationality: " +a.country[0].country_id + " with probability: " + a.country[0].probability + " or " + a.country[1].country_id + " with probability: " + a.country[1].probability;
-                var z = document.createTextNode(results);
-                output_results.appendChild(z);
-                output_results.classList.add("text-style");
-                output_results.classList.add("output-para");
-                document.body.appendChild(output_results);
+            const api_url2 = "https://api.genderize.io/?name="+new_name;
+            const gender = getapi(api_url2);
+            const getGender= () => {
+                gender.then((a) => {
+                    results =  "\nPredicted gender is: " +a.gender + " with probability: " + a.probability;
+                    var x = document.createTextNode(results);
+                    output_results.appendChild(x);
+                    
+                });
+            };
 
-            });
-        };
-        getNationality();
+            getGender();
+
+
+            const api_url3 = "https://api.nationalize.io/?name="+new_name;
+            const nationality = getapi(api_url3);
+            const getNationality= () => {
+                nationality.then((a) => {
+                    results =   "\nPredicted nationality: " +a.country[0].country_id + " with probability: " + a.country[0].probability + " or " + a.country[1].country_id + " with probability: " + a.country[1].probability;
+                    var z = document.createTextNode(results);
+                    output_results.appendChild(z);
+                    output_results.classList.add("text-style");
+                    output_results.classList.add("output-para");
+                    document.body.appendChild(output_results);
+
+                });
+            };
+            getNationality();
+
+        }
 
         
 
